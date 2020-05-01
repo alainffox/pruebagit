123b0710d8ad515858b675411df60ee848ffbe3b
alainffox <gra.alainhv@gmail.com>
2020-05-01 12:39:45 -0500

    guardarndo primer cambio

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Bienevienod a prueb GIT 3 - RAMA MASTER v2
+</body>
+</html>
